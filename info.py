1ecb1431f0be8129f00930b15de908076e08233a
imran12ap76 <77523586+imran12ap76@users.noreply.github.com>
2024-10-08 10:02:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID' '25132226'])
-API_HASH = environ['API_HASH' '43e35a9f9d711205b155b2c668c5dc22']
-BOT_TOKEN = environ['BOT_TOKEN' '5143948797:AAEKnjEi9YkbdZ7mT7tm0U8L7TMAJ8GL0Jo']
+API_ID = int(environ['API_ID', '25132226'])
+API_HASH = environ['API_HASH', '43e35a9f9d711205b155b2c668c5dc22']
+BOT_TOKEN = environ['BOT_TOKEN', '5143948797:AAEKnjEi9YkbdZ7mT7tm0U8L7TMAJ8GL0Jo']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -62,7 +62,7 @@ PORT = environ.get("PORT", "8080")
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+GOFte-Rz2tcxODg1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+mCMdCb_ymAowZmNl')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1002310112807))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'DQ_The_File_Donor_Support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
